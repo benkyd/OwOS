c211353c6ea6910890407def30bc77c27d9a1b4d
Benjamin Kyd <benjaminkyd@gmail.com>
2019-04-24 21:28:33 +0100

    OS Barebones done

---
+++ kernel.asm
@@ -0,0 +1,61 @@
+# OwOS
+# Copyright Benjamin Kyd (c) 2019
+
+# declare constants for the multiboot header
+.set ALIGN,    1<<0                     # align loaded modules on page boundaries
+.set MEMINFO,  1<<1                     # provide memory map
+.set FLAGS,    ALIGN | MEMINFO          # this is the Multiboot 'flag' field
+.set MAGIC,    0x1BADB002               #'magic number' lets bootloader find the header
+.set CHECKSUM, -(MAGIC + FLAGS)         # checksum of above, to prove we are multiboot
+
+.section .multiboot
+.align 4
+.long MAGIC
+.long FLAGS
+.long CHECKSUM
+ 
+.section .bss
+.align 16
+stack_bottom:
+.skip 16384   # 16 KiB
+stack_top:
+
+.section .text
+.global _start
+.type _start, @function
+
+_start:
+    # Settup stack
+	mov $stack_top, %esp
+	push %ebx
+	push %eax
+
+    # FPU Config
+    VAL_037F:
+        .hword 0x037F
+    VAL_037E:
+        .hword 0x037E
+    VAL_037A:
+        .hword 0x037A
+    # Configure FPU
+    cli
+    mov %cr0, %eax
+    or $0b00110010, %eax
+    and $0xFFFFFFFB, %eax
+    mov %eax, %cr0
+    fldcw VAL_037F
+    fldcw VAL_037E
+    fldcw VAL_037A
+    fninit
+
+	call kernel_main
+
+	cli
+1:	hlt
+	jmp 1b
+
+_hang:
+    hlt
+    jmp _hang
+
+.size _start, . - _start
